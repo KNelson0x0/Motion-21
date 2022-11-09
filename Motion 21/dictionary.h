2eaf138d7d59b5734750107574a395a775b44563
KNelson0x0 <67765481+KNelson0x0@users.noreply.github.com>
2022-11-08 21:21:34 -0800

    Bit of GUI organization. 90% of ASL_GUI.py is Keegan Grafs, just wasn't uploaded to the github yet. Animation basics and Camera basics.

--- Motion 21/dictionary.h
+++ Motion 21/dictionary.h
@@ -33,7 +33,7 @@ template<class T>
 struct is_ptr<T* const volatile> : std::true_type {};
 
 template <typename K, typename V>
-class Dictionary { // basically a wrapper around vector to emulate python dicts.
+class Dictionary { // basically a wrapper around vector to emulate python dicts. Please try and use primitive types, it gets yelly when you use stuff like str.
 public: // p-types
 	struct ENTRY { // basically just slimmed down pair
 	public: // p-methods
@@ -50,25 +50,74 @@ public: // p-methods, some overloads for extra flexibility
 
 	bool add(ENTRY entry) { return add(entry.key, entry.value); }
 	bool add(K key, V value) {
-		long our_null = 0xFFFFFFFFFFFFFFFF; // 0's will often need to be stored i'd assume, int maxs' will not. less room for error.
-		V result = (V)our_null;
+		V result = (V)dict_null;
 
-		if (is_ptr<V>::value) result = nullptr;
 		find(key, result);
-		if (is_ptr<V>::value) {
-			if (result == nullptr) {
-				dict.push_back({ key, value });
-				return true;
-			}
-		} else {
-			if (result == (V)our_null) {
-				dict.push_back({ key, value });
-				return true;
-			}
+		if (result == (V)dict_null) {
+			dict.push_back({ key, value });
+			return true;
 		}
 
 		debug_log("[Dictionary]> Key already exists.");
 		return false;
+	}
+	bool add(K* key, V* value) { // because it really did not like the is_ptrs after a while
+		V result = nullptr;
+
+		find(key, result);
+		if (result == nullptr) {
+			dict.push_back({ key, value });
+			return true;
+		}
+
+		debug_log("[Dictionary]> Key already exists.");
+		return false;
+	} // for some reason its smart enough to do this without the four overloads but isn't using the is_ptrs...
+	void display() {
+		cout << "Dictionary_Object: {\n";
+		for (ENTRY i : dict) cout << "\t" << i.key << " : " << i.value << ",\n";
+		cout << "}\n";
+	}
+	void find(K key, V& out_value) {
+		for (ENTRY i : dict) { // avoidin dem autos
+			if (i.key == key) {
+				out_value = i.value;
+				return;
+			};
+		}
+	}
+	void find(V value, K& out_key) const {
+		for (ENTRY i : dict) { 
+			if (i.value == value) {
+				out_key = i.key;
+				return;
+			};
+		}
+	}
+	int get_index(ENTRY entry) { return get_index(entry.key); }
+	int get_index(K key) {
+		for (int i{}; i < dict.size(); i++)
+			if (dict[i].key == key) return i;
+
+		debug_log("[Dictionary]> Nothing found");
+		return -1;
+	}
+	bool has(K key) {
+		V out = (V)dict_null;
+		find(key, out);
+		if (out == dict_null) return false;
+		else return true;
+	}
+	bool has(V value) const { // same uniquer with the const here
+		K out = (K)dict_null;
+		find(value, out);
+		if (out == dict_null) return false;
+		else return true;
+	}
+	vector<K> keys() {
+		vector<K> key_list;
+		for (int i{}; i < size(); i++) { key_list.push_back(dict[i].key); }
+		return key_list;
 	}
 	bool remove(ENTRY entry) { return remove(entry.key); }
 	bool remove(K key) {
@@ -83,6 +132,9 @@ public: // p-methods, some overloads for extra flexibility
 		debug_log("[Dictionary]> Removed key");
 		return true;
 	}
+	int size() {
+		return (int)dict.size();
+	}
 	bool update(ENTRY entry) { return update(entry.key, entry.value); }
 	bool update(K key, V new_value) {
 		int result = get_index(key);
@@ -96,40 +148,26 @@ public: // p-methods, some overloads for extra flexibility
 
 		return false;
 	}
-	void find(K key, V& out_value) {
-		for (ENTRY i : dict) { // avoidin dem autos
-			if (i.key == key) {
-				out_value = i.value;
-				return;
-			};
-		}
-	}
-	int get_index(ENTRY entry) { return get_index(entry.key); }
-	int get_index(K key) {
-		for (int i{}; i < dict.size(); i++)
-			if (dict[i].key == key) return i;
+	vector<V> values() {
+		vector<V> values_list;
+		for (int i{}; i < size(); i++) { values_list.push_back(dict[i].value); }
+		return values_list;
+	};
 
-		debug_log("[Dictionary]> Nothing found");
-		return -1;
-	}
-	void display() {
-		cout << "Dictionary_Object: {\n";
-		for (ENTRY i : dict) cout << "\t" << i.key << " : " << i.value << ",\n";
-		cout << "}\n";
-	}
-
-	V& operator[](K key) {
-		for (ENTRY& i : dict) { // avoidin dem autos
-			if (i.key == key) {
-				return i.value;
-			};
-		}
-	}
-	ENTRY& operator[](int index) {
+	ENTRY& operator[](int index) const { // moved const to this line until i get back and write a get
 		return ((ENTRY&)dict[index]);
 	}
+	V& operator[](K key) { // use of const here is to avoid "differs only by return type", gonna have to convert to a .get if this keeps providing problems 
+		for (ENTRY& i : dict) { // avoidin dem autos
+			if (i.key == key) 
+				return i.value;
+		}
+	}
+
 private: // pv-attributes
 	vector<ENTRY> dict;
+public: // p-attributes
+	long dict_null = 0xEEEEEEEEEEEEEEE;  // 0s or nullptrs will often need to be stored i'd assume, large numbers will not. less room for error.
 };
 
 template <typename K, typename V> // didn't want to have to always type out "Dictionary"
--- Motion 21/main.cpp
+++ Motion 21/main.cpp
@@ -1,8 +1,51 @@
 // APPLICATION MAIN
+#define CIRCULAR_IMPORTS
+#include "includes.h"
+#include "tools.h"
+#include "dictionary.h"
+#include "solutionbase.h"
+#include "none.h"
+
+//#include "calculator_graph.h"
+
 
 
 
 int main() {
+
+	int a = 1, b = 2;
+	int* a_ptr = &a, * b_ptr = &b;
+
+
+	SolutionBase cool;
+	Dict<int*, int> test(a_ptr, -1);
+
+	test.add(b_ptr, b);
+
+	cout << "start\n";
+
+	unsigned long x = 696969;
+	cout << "uhhh\n";
+	if (a == x) cout << "swaggus\n";
+
+	unsigned long* swag1 = &x;
+	unsigned long* swag2 = &x;
+
+	cout << "swag1: " << &swag1 << "\n";
+	cout << "swag2: " << &swag2 << "\n";
+
+	unsigned long* swag2_addy = (unsigned long*)(&swag2);
+
+	cout << "swag2_addy_ptr: " << swag2_addy << "\n";
+	if (swag2 == *(unsigned long**)swag2_addy) cout << "swaggus2\n";
+
+	cout << "end\n";
+
+
+	test.display();
+	cout << split("Testing.This.Is.Much.More.Annoying.In.C", ".")[3] << "\n";	
+
+	cout << "end\n";
 
 
 	return 0;
