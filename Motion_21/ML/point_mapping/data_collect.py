694e392a429162298f7025b2507d3ccf9dfbd8f0
cjosim <chad.josim@gmail.com>
2023-04-11 20:38:42 -0700

    Added data_collect and generate_files. Added folders for base_letters and hand_images

---
+++ Motion_21/ML/point_mapping/data_collect.py
@@ -0,0 +1,40 @@
+import cv2
+import shutil
+import os
+import uuid
+
+################################# CHANGE THIS BASED ON WHICH LETTER YOU ARE DOING ####################################
+letter = 'D'
+
+
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,360)
+#cap.set(10,100) #for brightness
+
+dir = os.path.dirname(__file__) #directory of this file
+#print(dir)
+path = dir + '\hand_images_' + letter
+
+if os.path.exists(path):
+    shutil.rmtree(path)
+os.mkdir(path)
+
+counter = 0
+
+while True:
+
+    success, img = cap.read()
+
+    img = cv2.rectangle(img, (100,100), (300,300), (0,255,0), 2) #image, start_point, end_point, color, thickness
+    crop_img = img[100:300, 100:300]
+
+    cv2.imshow("Image", img)
+    cv2.imshow("Cropped Image", crop_img)
+
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(os.path.join(path, '{}.jpg'.format(uuid.uuid1())), crop_img)
+        print(counter)
