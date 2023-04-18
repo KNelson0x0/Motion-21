14cb448c76f6c271e7e69f5793f1c975bf9e89d3
reallyseb <sebastianspike@gmail.com>
2023-04-18 14:06:54 -0700

    importing working title screen

--- otherGUI/ASL_updated.py
+++ otherGUI/ASL_updated.py
@@ -27,6 +27,7 @@ class App(customtkinter.CTk):
         self.geometry("740x520")
         self.title("ASL Learning App")
 
+
         #Handy closing function to stop all running processes even when window is closed
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
@@ -50,44 +51,46 @@ class App(customtkinter.CTk):
 
     # Creates the home window
     def home_window(self):
-        self.grid_columnconfigure(1, weight=1)
+        #self.grid_columnconfigure(1, weight=1)
+        #self.grid_rowconfigure(0, weight=1)
 
 
-        self.home_image = self.load_image("/images/MainBG.png", 740, 520)
-        self.left_finger = self.load_image("/images/finger2.png", 80, 80)
-        self.right_finger = self.load_image("/images/finger.png", 80, 80)
-
-        self.background_image = tkinter.Label(master=self, image=self.home_image)
-        self.background_image.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-        self.frame_right = customtkinter.CTkFrame(master = self)
-        self.frame_left = customtkinter.CTkFrame(master = self)
-
-        #self.frame_right.grid(row = 0, column = 1, padx = 20, pady = 2)
+        #self.bg_image = customtkinter.CTkImage(Image.open(PATH + "/images/blackbg.png"), size=(740, 520))
+        #self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image, text = "")
+        #self.bg_image_label.place(relx= 0, rely = 0)
 
 
+        self.frame_middle = customtkinter.CTkFrame(master = self, border_width= 3)
+        self.frame_left = customtkinter.CTkFrame(master = self, height = 40, border_width= 3)
+        self.frame_right = customtkinter.CTkFrame(master = self, height = 40, border_width= 3)
 
-        self.motion21_title = customtkinter.CTkLabel(master=self, text = "MOTION 21", corner_radius = 0, width = 20, height = 20, font = ("Segoe UI", 100, "bold"), fg_color=("black","black"))
-        self.motion21_title.grid(row=0, column=1, padx=0, pady=5, sticky = "n")
-
-        self.button1 = customtkinter.CTkButton(master=self, text = "Lesson Select", font = ("Seoue UI", 50, "bold"), width = 200, height = 50, border_width = 2, corner_radius = 8,  fg_color = "grey", border_color="#000000")
-        self.button1.grid(row=1, column=1, padx=0, pady=5, sticky = "n")
-
-        self.button2 = customtkinter.CTkButton(master=self, text = "Settings", font = ("Seoue UI", 50, "bold"), width = 200, height = 50, border_width = 2, corner_radius = 8,  fg_color = "grey", border_color="#000000")
-        self.button2.grid(row=2, column=1, padx=0, pady=5, sticky = "n")
-
-        self.button2 = customtkinter.CTkButton(master=self, text = "Exit", font = ("Seoue UI", 50, "bold"), width = 200, height = 50, border_width = 2, corner_radius = 8,  fg_color = "grey", border_color="#000000")
-        self.button2.grid(row=3, column=1, padx=0, pady=5, sticky = "n")
-
-        self.left_image = tkinter.Label(master = self, image = self.left_finger)
-        self.left_image.grid(row = 0, column = 0)
-
-        self.right_image = tkinter.Label(master = self, image = self.right_finger)
-        self.right_image.grid(row = 0, column = 3)
+        self.frame_middle.grid(row = 0, column = 0, padx = 20, pady = 10)
+        self.frame_left.place(relx = 0.08, rely = .85)
+        self.frame_right.grid(row = 0, column = 1, padx = 0, pady = 10)
 
 
 
 
+        self.motion21_title = customtkinter.CTkLabel(master=self.frame_middle, text = "MOTION 21", corner_radius = 10, width = 20, height = 20, font = ("Segoe UI", 70, "bold"))
+        self.motion21_title.grid(row=0, column=0, padx=5, pady=5, sticky = "n")
+
+        self.button1 = customtkinter.CTkButton(master=self.frame_middle, text = "Lesson Select", font = ("Seoue UI", 50, "bold"), width = 100, height = 50, border_width = 3, corner_radius = 10, border_color="#000000")
+        self.button1.grid(row=1, column=0, padx=0, pady=10, sticky = "n")
+
+        self.button2 = customtkinter.CTkButton(master=self.frame_left, text = "Settings", font = ("Seoue UI", 12, "bold"), width = 200, height = 50, border_width = 3, corner_radius = 10, border_color="#000000")
+        self.button2.grid(row=0, column=0, padx=2, pady=5, sticky = "n")
+
+        self.button3 = customtkinter.CTkButton(master=self.frame_left, text = "Logout", font = ("Seoue UI", 12, "bold"), width = 200, height = 50, border_width = 3, corner_radius = 10, border_color="#000000")
+        self.button3.grid(row=0, column=1, padx=2, pady=5, sticky = "n")
+
+        self.button4 = customtkinter.CTkButton(master=self.frame_left, text = "Exit", font = ("Seoue UI", 12, "bold"), width = 200, height = 50, border_width = 3, corner_radius = 10, border_color="#000000")
+        self.button4.grid(row=0, column=2, padx=2, pady=5, sticky = "n")
+
+        self.label = customtkinter.CTkLabel(master = self.frame_right, text = "Testing\n\n\n", corner_radius = 10, width = 20, height = 20, font = ("Segoe UI", 70, "bold"))
+        self.label.grid(row = 0, column = 0, padx =0)
+
+        self.label1 = customtkinter.CTkLabel(master = self, text = "Testing\n\n", corner_radius = 10, width = 20, height = 20, font = ("Segoe UI", 12, "bold"))
+        self.label1.grid(row = 1, column = 0, padx =0)
 
 
 
